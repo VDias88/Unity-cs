cb6602b7e81940a3d402ebc7901722c5ed32d2f6
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2020-11-14 20:49:39 -0300

    Comunicação basica
    comunicação basica entre forms
    usado para treino

---
+++ WinForms/WindForms/WindForms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Ola");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txt = textBox1.Text;
+            Form2 f1 = new Form2(txt);
+            f1.Show();
+        }
+    }
+}
