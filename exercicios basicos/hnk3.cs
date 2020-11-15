679b8935ecb471aea229012549ec14406e641608
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2020-11-15 01:23:38 -0300

    Update hnk3.cs

--- exercicios basicos/hnk3.cs
+++ exercicios basicos/hnk3.cs
@@ -12,7 +12,10 @@ class Result
 class hnk3{
     static void Main(){
         List<List<int>> lisad=new List<List<int>>();
-       
+       for(int i=0;i<=lisad.Count-1;i++){
+           List<int> integ=new List<int>();
+           
+       }
 
     }
 }
