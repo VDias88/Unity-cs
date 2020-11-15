796881c8f250c531ad9bbe402830a566af356235
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2020-11-15 17:27:15 -0300

    hackerhank3
    another hackerhank problem solving
    this time using Lists of List and diagonal

--- exercicios basicos/hnk3.cs
+++ exercicios basicos/hnk3.cs
@@ -1,21 +1,53 @@
 using System;
 using System.Collections.Generic;
-//code with nested lists in c# and some of values
+//code with nested lists in c# and some of 2 diagonal values
 class Result
 {
-    /*public static int diagonalDifference(List<List<int>> arr)
-    {
-
-    }*/
+    public static int diagonalDifference(List<List<int>> arr)
+    {   
+        int sublists=0;
+        int some1=0;
+        int some2=0;
+        foreach (var sublist in arr)
+        {
+            sublists+=1;
+        }
+        int j=0;
+        foreach (var sublist in arr)
+        { 
+            for(int i=0;i<=sublists;i++){
+               if(i==j){
+                   some1=sublist[i]+some1;
+               }
+            }
+            j++;
+        }
+        int j1=0;
+        foreach (var sublist in arr)
+        { 
+            for(int i=0;i<=sublists-1;i++){
+                
+               if(i+j1==sublists-1){
+                   
+                   some2=sublist[i]+some2;
+                   
+               }
+               
+            }
+           j1+=1;  
+        }
+        return Math.Abs(some1-some2);
+    }
 
 }
 class hnk3{
     static void Main(){
-        List<List<int>> lisad=new List<List<int>>();
-       for(int i=0;i<=lisad.Count-1;i++){
-           List<int> integ=new List<int>();
-           
-       }
-
+        Result a1=new Result();
+        List<List<int>> asd=new List<List<int>>();
+        asd.Add(new List<int> { 11, 2, 4 });
+        asd.Add(new List<int> { 4, 5, 6 });
+        asd.Add(new List<int> { 10, 8, -12 });
+        Console.WriteLine(Result.diagonalDifference(asd));
+        //Result.diagonalDifference(asd);
     }
 }
