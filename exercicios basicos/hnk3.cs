4933bec7a50263e4110d48edb9eb737fca0ace79
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2020-11-14 22:10:25 -0300

    Create hnk3.cs

---
+++ exercicios basicos/hnk3.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+//code with nested lists in c# and some of values
+class Result
+{
+    /*public static int diagonalDifference(List<List<int>> arr)
+    {
+
+    }*/
+
+}
+class hnk3{
+    static void Main(){
+        List<List<int>> lisad=new List<List<int>>();
+       
+
+    }
+}
