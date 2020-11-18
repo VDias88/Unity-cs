0686b6ac14a9bf3aafcf679fc61cc200f9248ff9
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2020-11-18 17:48:50 -0300

    conversion 12 to 24 tipe hour
    simple code hackerhank

--- exercicios basicos/hnk8.cs
+++ exercicios basicos/hnk8.cs
@@ -1,16 +1,23 @@
 using System;
-
+//Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 class hnk8{
-    static void timeConversion(string s) {
-
-        //string dat1=s.Substring(0,s.Length-2);
-        string dat2=(s.Split('P')).ToString();
-        Console.WriteLine(dat2);
-        //DateTime dat=Convert.ToDateTime(dat1);
-        
+    /*
+    for some reason this code isnt working in the hackerhank 
+    static string timeConversion(string s) {
+        String dat1=s.Substring(0,s.Length-2);
+        String dat2=s.Substring(s.Length-2,2);
+        String dat3=dat1+" "+dat2;
+        DateTime dat=DateTime.Parse(dat3);
+        String result=dat.ToString();
+        return(result.Substring(11,8));
+    }*/
+    //the code bellow works
+    static string timeConversion(string s) {
+       DateTime time=DateTime.Parse(s);
+        return(time.ToString("HH:mm:ss"));
     }
-    static void Main(){
+    static void Main(string[] args){
         string h="07:05:45PM";
-        timeConversion(h);
+        Console.WriteLine(timeConversion(h));
     }
 }
