7b2aa78dea8f7db78ac68d315a7bd4301ecaca5d
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2020-11-19 18:54:03 -0300

    Update hnk10.cs

--- exercicios basicos/hnk10.cs
+++ exercicios basicos/hnk10.cs
@@ -1,14 +1,37 @@
 using System;
 
 class hnk10{
-    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
         //s and t representing the area of the house
         //a and b representing the tree location
         //apples and oranges representing the lounch range
+        //output the number of apples and oranges that fall on the house
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int falapples=0;
+        int falorang=0;
         
+        for(int i=0;i<=apples.Length-1;i++){
+            if(apples[i]<0){
 
+            }
+            else if((a+apples[i])>=s){
+                falapples++;
+            }
+            
+        }
+        for(int i=0;i<=oranges.Length-1;i++){
+            if(oranges[i]>0){
+
+            }
+           else if((b-(Math.Abs(oranges[i])))<=t){
+                falorang++;
+            }
+        }
+        Console.WriteLine(falapples);
+        Console.WriteLine(falorang);
     }
     static void Main(){
-        
+        int[] apples={2};
+        int [] oranges={-2};
+        countApplesAndOranges(2,3,1,5,apples,oranges);
     }
 }
