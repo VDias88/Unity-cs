4855ec282ce246779769b32144249dca1c6d9649
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2020-11-19 18:03:25 -0300

    Create hnk10.cs

---
+++ exercicios basicos/hnk10.cs
@@ -0,0 +1,14 @@
+using System;
+
+class hnk10{
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        //s and t representing the area of the house
+        //a and b representing the tree location
+        //apples and oranges representing the lounch range
+        
+
+    }
+    static void Main(){
+        
+    }
+}
